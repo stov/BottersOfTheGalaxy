e62d74ce6a225fcb7a384d646fc26fd6f0dc5e18
Chris Bateup <github@chrisbateup.com>
2018-03-03 05:53:17 +1100

    Setup repository and new console app

---
+++ src/BottersOfTheGalaxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BottersOfTheGalaxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
